38773b39bf3bd14e3e34da1dace75ff5f7440919
Luiz1502 <luizguluizas@gmail.com>
2025-02-11 14:51:24 -0300

    sla

---
+++ pikachu.php
@@ -0,0 +1,33 @@
+<?php include './header.php';?>
+<div class="container">
+  <div class="row d-flex justify-content-center">
+    <div class="col-10 p-3">
+
+<div id="carouselExample" class="carousel slide">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="https://i.scdn.co/image/ab67616d0000b273cfeae645958e9248abff0710" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="https://i.scdn.co/image/ab67616d0000b273cfeae645958e9248abff0710" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="https://www.google.com/imgres?q=pikachu&imgurl=https%3A%2F%2Fconteudo.imguol.com.br%2Fc%2Fentretenimento%2F58%2F2017%2F05%2F30%2Fpikachu-nervoso-1496159464346_v2_615x300.png&imgrefurl=https%3A%2F%2Fwww.bol.uol.com.br%2Flistas%2Fveja-10-coisas-sobre-o-pikachu-que-talvez-voce-nao-saiba.htm&docid=NiOXzhd9gm81_M&tbnid=nNurgBcI8w3SXM&vet=12ahUKEwiTvY3-lbyLAxWTqJUCHQxCFeEQM3oECD4QAA..i&w=615&h=300&hcb=2&ved=2ahUKEwiTvY3-lbyLAxWTqJUCHQxCFeEQM3oECD4QAA" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
+  </div>
+</div>
+
+
+
+<?php include './footer.php';?>
